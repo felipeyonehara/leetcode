5608ea57978a292bd6c38116c687ec70f03944b5
Felipe Yonehara <felipeyonehara@gmail.com>
2021-08-04 11:26:16 -0300

    baseball game weird score

---
+++ baseball-game.py
@@ -0,0 +1,15 @@
+def calPoints(ops) -> int:
+    
+    result = None
+    lista = list()
+    for i in range(len(ops)):
+        if( ops[i] == "+"): 
+            print("+")
+            result.append(ops[i-1] + ops[i-2])
+            print(result)
+        if( ops[i] == "D"): 
+            print("D")
+            result += 
+            lista.index
+            
+    return result
